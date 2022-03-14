125824b4ee2ade0e1bc8062b32d779dda0350801
배나영 <baenayoung@baenayoung-ui-MacBook-Pro.local>
2022-03-14 17:11:04 +0800

    part8

--- blockchain/merkle.go
+++ blockchain/merkle.go
@@ -1,247 +1,59 @@
 package blockchain
 
-import (
-	"bytes"
-	"crypto/ecdsa"
-	"encoding/hex"
-	"errors"
-	"fmt"
-	"log"
-	"os"
-	"runtime"
+import "crypto/sha256"
 
-	"github.com/dgraph-io/badger"
-)
-
-const (
-	dbPath      = "./tmp/blocks"
-	dbFile      = "./tmp/blocks/MANIFEST"
-	genesisData = "First Transaction from Genesis"
-)
-
-type BlockChain struct {
-	LastHash []byte
-	Database *badger.DB
+type MerkleTree struct {
+	RootNode *MerkleNode
 }
 
-type BlockChainIterator struct {
-	CurrentHash []byte
-	Database    *badger.DB
+type MerkleNode struct {
+	Left  *MerkleNode
+	Right *MerkleNode
+	Data  []byte
 }
 
-func DBexists() bool {
-	if _, err := os.Stat(dbFile); os.IsNotExist(err) {
-		return false
+func NewMerkleNode(left, right *MerkleNode, data []byte) *MerkleNode {
+	node := MerkleNode{}
+
+	if left == nil && right == nil {
+		hash := sha256.Sum256(data)
+		node.Data = hash[:]
+	} else {
+		prevHashes := append(left.Data, right.Data...)
+		hash := sha256.Sum256(prevHashes)
+		node.Data = hash[:]
 	}
 
-	return true
+	node.Left = left
+	node.Right = right
+
+	return &node
 }
 
-func ContinueBlockChain(address string) *BlockChain {
-	if DBexists() == false {
-		fmt.Println("No existing blockchain found, create one!")
-		runtime.Goexit()
+func NewMerkleTree(data [][]byte) *MerkleTree {
+	var nodes []MerkleNode
+
+	if len(data)%2 != 0 {
+		data = append(data, data[len(data)-1])
 	}
 
-	var lastHash []byte
-
-	opts := badger.DefaultOptions
-	opts.Dir = dbPath
-	opts.ValueDir = dbPath
-
-	db, err := badger.Open(opts)
-	Handle(err)
-
-	err = db.Update(func(txn *badger.Txn) error {
-		item, err := txn.Get([]byte("lh"))
-		Handle(err)
-		lastHash, err = item.Value()
-
-		return err
-	})
-	Handle(err)
-
-	chain := BlockChain{lastHash, db}
-
-	return &chain
-}
-
-func InitBlockChain(address string) *BlockChain {
-	var lastHash []byte
-
-	if DBexists() {
-		fmt.Println("Blockchain already exists")
-		runtime.Goexit()
+	for _, dat := range data {
+		node := NewMerkleNode(nil, nil, dat)
+		nodes = append(nodes, *node)
 	}
 
-	opts := badger.DefaultOptions
-	opts.Dir = dbPath
-	opts.ValueDir = dbPath
+	for i := 0; i < len(data)/2; i++ {
+		var level []MerkleNode
 
-	db, err := badger.Open(opts)
-	Handle(err)
-
-	err = db.Update(func(txn *badger.Txn) error {
-		cbtx := CoinbaseTx(address, genesisData)
-		genesis := Genesis(cbtx)
-		fmt.Println("Genesis created")
-		err = txn.Set(genesis.Hash, genesis.Serialize())
-		Handle(err)
-		err = txn.Set([]byte("lh"), genesis.Hash)
-
-		lastHash = genesis.Hash
-
-		return err
-
-	})
-
-	Handle(err)
-
-	blockchain := BlockChain{lastHash, db}
-	return &blockchain
-}
-
-func (chain *BlockChain) AddBlock(transactions []*Transaction) *Block {
-	var lastHash []byte
-
-	for _, tx := range transactions {
-		if chain.VerifyTransaction(tx) != true {
-			log.Panic("Invalid Transaction")
-		}
-	}
-
-	err := chain.Database.View(func(txn *badger.Txn) error {
-		item, err := txn.Get([]byte("lh"))
-		Handle(err)
-		lastHash, err = item.Value()
-
-		return err
-	})
-	Handle(err)
-
-	newBlock := CreateBlock(transactions, lastHash)
-
-	err = chain.Database.Update(func(txn *badger.Txn) error {
-		err := txn.Set(newBlock.Hash, newBlock.Serialize())
-		Handle(err)
-		err = txn.Set([]byte("lh"), newBlock.Hash)
-
-		chain.LastHash = newBlock.Hash
-
-		return err
-	})
-	Handle(err)
-
-	return newBlock
-}
-
-func (chain *BlockChain) Iterator() *BlockChainIterator {
-	iter := &BlockChainIterator{chain.LastHash, chain.Database}
-
-	return iter
-}
-
-func (iter *BlockChainIterator) Next() *Block {
-	var block *Block
-
-	err := iter.Database.View(func(txn *badger.Txn) error {
-		item, err := txn.Get(iter.CurrentHash)
-		Handle(err)
-		encodedBlock, err := item.Value()
-		block = Deserialize(encodedBlock)
-
-		return err
-	})
-	Handle(err)
-
-	iter.CurrentHash = block.PrevHash
-
-	return block
-}
-
-func (chain *BlockChain) FindUTXO() map[string]TxOutputs {
-	UTXO := make(map[string]TxOutputs)
-	spentTXOs := make(map[string][]int)
-
-	iter := chain.Iterator()
-
-	for {
-		block := iter.Next()
-
-		for _, tx := range block.Transactions {
-			txID := hex.EncodeToString(tx.ID)
-
-		Outputs:
-			for outIdx, out := range tx.Outputs {
-				if spentTXOs[txID] != nil {
-					for _, spentOut := range spentTXOs[txID] {
-						if spentOut == outIdx {
-							continue Outputs
-						}
-					}
-				}
-				outs := UTXO[txID]
-				outs.Outputs = append(outs.Outputs, out)
-				UTXO[txID] = outs
-			}
-			if tx.IsCoinbase() == false {
-				for _, in := range tx.Inputs {
-					inTxID := hex.EncodeToString(in.ID)
-					spentTXOs[inTxID] = append(spentTXOs[inTxID], in.Out)
-				}
-			}
+		for j := 0; j < len(nodes); j += 2 {
+			node := NewMerkleNode(&nodes[j], &nodes[j+1], nil)
+			level = append(level, *node)
 		}
 
-		if len(block.PrevHash) == 0 {
-			break
-		}
-	}
-	return UTXO
-}
-
-func (bc *BlockChain) FindTransaction(ID []byte) (Transaction, error) {
-	iter := bc.Iterator()
-
-	for {
-		block := iter.Next()
-
-		for _, tx := range block.Transactions {
-			if bytes.Compare(tx.ID, ID) == 0 {
-				return *tx, nil
-			}
-		}
-
-		if len(block.PrevHash) == 0 {
-			break
-		}
+		nodes = level
 	}
 
-	return Transaction{}, errors.New("Transaction does not exist")
-}
+	tree := MerkleTree{&nodes[0]}
 
-func (bc *BlockChain) SignTransaction(tx *Transaction, privKey ecdsa.PrivateKey) {
-	prevTXs := make(map[string]Transaction)
-
-	for _, in := range tx.Inputs {
-		prevTX, err := bc.FindTransaction(in.ID)
-		Handle(err)
-		prevTXs[hex.EncodeToString(prevTX.ID)] = prevTX
-	}
-
-	tx.Sign(privKey, prevTXs)
-}
-
-func (bc *BlockChain) VerifyTransaction(tx *Transaction) bool {
-	if tx.IsCoinbase() {
-		return true
-	}
-	prevTXs := make(map[string]Transaction)
-
-	for _, in := range tx.Inputs {
-		prevTX, err := bc.FindTransaction(in.ID)
-		Handle(err)
-		prevTXs[hex.EncodeToString(prevTX.ID)] = prevTX
-	}
-
-	return tx.Verify(prevTXs)
+	return &tree
 }
